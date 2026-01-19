4844456f22c4263d1319917e267fc155e0115929
MagicGopher <gopher997@gmail.com>
2026-01-19 23:50:46 +0800

    feat(http): http示例。
    - 模拟client/server端示例。

---
+++ 02_advanced/04_http/demo01/client/main.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+)
+
+// 客户端
+// 发起http请求（get请求）服务器获取数据
+
+func main() {
+	// 定义请求携带的参数
+	param := "Hello"
+	// 定义请求的url路径
+	url := fmt.Sprintf("http://127.0.0.1:8080/hello?value=%s", param)
+	// 发起GET请求
+	resp, err := http.Get(url)
+	if err != nil {
+		log.Printf("")
+	}
+	// defer关闭响应
+	defer func() {
+		if closeErr := resp.Body.Close(); closeErr != nil {
+			log.Printf("响应关闭失败: %v\n", closeErr)
+		} else {
+			log.Println("响应关闭成功！")
+		}
+	}()
+	// 检查响应状态码（通常先判断是不是200）
+	if resp.StatusCode != http.StatusOK {
+		log.Printf("服务器返回了错误状态码: %d %s", resp.StatusCode, resp.Status)
+		return
+	}
+	// 读取服务器返回的内容
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		log.Printf("读取响应内容失败: %v", err)
+		return
+	}
+	// 输出响应状态码和服务端响应的内容
+	fmt.Printf("服务器返回的状态码: %v\n", resp.StatusCode)
+	fmt.Printf("服务器返回的内容: %v\n", string(body))
+}
---
+++ 02_advanced/04_http/demo01/server/main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+// 服务端
+// 处理客户端的请求已经响应数据
+
+// Hello 接收客户端查询参数，例如：/xxx?value=xxx
+func Hello(w http.ResponseWriter, r *http.Request) {
+	// 只接收GET请求
+	if r.Method != "GET" {
+		http.Error(w, "请求方法有误！", http.StatusMethodNotAllowed)
+	}
+	// 定义默认结果
+	value := "Hi, this is the server."
+	// 解析参数
+	value = r.URL.Query().Get("value")
+	// 拼接响应字符内容
+	content := fmt.Sprintf("Hi, %s!", value)
+	// 响应请求
+	write, err := w.Write([]byte(content))
+	if err != nil {
+		log.Printf("响应写入内容失败: %v\n", err)
+		return
+	}
+	fmt.Printf("成功写入响应 %d 字节\n", write)
+}
+
+func main() {
+	// 定义路由接口
+	http.HandleFunc("/hello", Hello)
+	log.Println("服务端启动成功")
+	// 启动服务端并监听8080端口
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		log.Fatalf("服务端启动失败: %v", err)
+	}
+}

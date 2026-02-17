266a386763ad4c02b8bed4365634e5022c620fa3
MagicGopher <gopher997@gmail.com>
2026-02-17 23:47:42 +0800

    feat(advanced): time标准库示例。
    - 获取时分秒。
    - 创建特定的time实例。

--- 02_advanced/08_time/time_test.go
+++ 02_advanced/08_time/time_test.go
@@ -17,11 +17,26 @@ func TestGetWeekday(t *testing.T) {
 	t.Log(now.Weekday())
 }
 
-// TestGetYear 获取年月日
-func TestGetYear(t *testing.T) {
+// TestGetYearMonthDay 获取年月日
+func TestGetYearMonthDay(t *testing.T) {
 	now := time.Now()    // 获取本地时间，这里会返回一个 Time
 	year := now.Year()   // 获取年份
 	month := now.Month() // 获取月份
 	day := now.Day()     // 获取日期
-	t.Logf("%v 年 %v 月 %v 日", year, month, day)
+	t.Logf("%v 年 %v 月 %v 日\n", year, month, day)
+}
+
+// TestGetHourMinuteSecond 获取时分秒
+func TestGetHourMinuteSecond(t *testing.T) {
+	now := time.Now()      // 获取本地时间，这里会返回一个 Time
+	hour := now.Hour()     // 获取当前小时
+	minute := now.Minute() // 获取分钟
+	second := now.Second() // 获取秒
+	t.Logf("%v 时 %v 分 %v 秒\n", hour, minute, second)
+}
+
+// TestGetTime 创建一个特定的Time实例
+func TestGetTime(t *testing.T) {
+	date := time.Date(2020, 2, 2, 10, 15, 45, 0, time.Local)
+	t.Log(date)
 }

fc0c7bcd148f8259e97b032bac47ae0b9c67d6b7
MagicGopher <gopher997@gmail.com>
2026-02-18 22:46:17 +0800

    feat(advanced): time标准库示例。
    - 时间解析示例。
    - 字符串格式时间转成time格式示例。
    - 时间增加/减少示例。
    - 两个时间的差值示例。

--- 02_advanced/08_time/time_test.go
+++ 02_advanced/08_time/time_test.go
@@ -40,3 +40,42 @@ func TestGetTime(t *testing.T) {
 	date := time.Date(2020, 2, 2, 10, 15, 45, 0, time.Local)
 	t.Log(date)
 }
+
+// TestParseTime 将字符串格式时间解析为Time
+func TestParseTime(t *testing.T) {
+	timeStr1 := "2022-01-30 21:00:00"
+	//timeStr1 := "Hello, World！"
+	parse, err := time.Parse("2006-01-02 15:04:05", timeStr1)
+	if err != nil {
+		t.Fatalf("解析时间错误: %v\n", err)
+	}
+	t.Logf("解析后的时间: %v\n", parse)
+}
+
+// TestTimeFormat 将time转为字符串格式时间
+func TestTimeFormat(t *testing.T) {
+	date := time.Date(2020, 2, 2, 10, 15, 45, 0, time.Local)
+	format := date.Format("2006年~01月~02日 15点04分05秒")
+	t.Logf("类型: %T, 值: %v\n", format, format)
+}
+
+// TestAddTime 增加或者减少时间
+func TestAddTime(t *testing.T) {
+	date1 := time.Date(2026, 2, 1, 10, 0, 0, 0, time.Local)
+	resultTime1 := date1.Add(-3 * time.Hour) // date1时间减三小时
+	t.Logf("%v 3小时前的时间是: %v\n", date1, resultTime1)
+	resultTime2 := date1.Add(5*time.Hour + 25*time.Minute)
+	t.Logf("%v 5小时25分后的时间是: %v\n", date1, resultTime2)
+}
+
+// TestSubTime 两个时间的差值
+func TestSubTime(t *testing.T) {
+	t1 := time.Now()
+	date1 := t1.Add(-3 * time.Hour) // 3小时前
+	date2 := t1.Add(3 * time.Hour)  // 3小时后
+	// “从过去到现在” → 现在.Sub(过去) → 正
+	// “从现在到过去” → 过去.Sub(现在) → 负
+	// “谁晚谁放前面，谁早谁放括号里”
+	sub := date1.Sub(date2) // 从 date2 到 date1 过了多久？
+	t.Log(sub)
+}
